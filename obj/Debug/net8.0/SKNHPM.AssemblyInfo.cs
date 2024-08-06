91e103a9cf546e4d9dec15813b4608afaa09c48e
nuch0208 <nutchanet2887@gmail.com>
2024-08-06 08:46:04 +0700

    update color

--- obj/Debug/net8.0/SKNHPM.AssemblyInfo.cs
+++ obj/Debug/net8.0/SKNHPM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SKNHPM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a437c7cfaac6063162e0d30c61eaa5ebedcfb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d191d179a5d6e181f2fc2d375df75c5987663a65")]
 [assembly: System.Reflection.AssemblyProductAttribute("SKNHPM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SKNHPM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
